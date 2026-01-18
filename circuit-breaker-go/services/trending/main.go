601c0ae1f404da224921695ff9053804d220681f
Aneesh Ahuja <aneeshahuja31@gmail.com>
2026-01-18 10:31:31 +0530

    Refactor code formatting and improve readability in main function

--- circuit-breaker-go/services/trending/main.go
+++ circuit-breaker-go/services/trending/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+
 	//"io"
 	"net/http"
 	"os"
@@ -13,41 +14,40 @@ import (
 )
 
 type post struct {
-	ID int64 `json64:"id"`
-	Email string `json:"email"`
-	Content string `json:"content"`
-	Views int `json:"views"`
+	ID        int64     `json:"id"`
+	Email     string    `json:"email"`
+	Content   string    `json:"content"`
+	Views     int       `json:"views"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
-func main(){
+func main() {
 	godotenv.Load()
 	router := gin.Default()
-	router.GET("/trending",func(ctx *gin.Context) {
-		POSTSAPI_URL,ok := os.LookupEnv("POSTSAPI_URL")
+	router.GET("/trending", func(ctx *gin.Context) {
+		POSTSAPI_URL, ok := os.LookupEnv("POSTSAPI_URL")
 		if !ok {
 			POSTSAPI_URL = "http://localhost:7001"
 		}
-		resp,err := http.Get(POSTSAPI_URL+"/topPosts?limit=5")
-		if err != nil{
-			ctx.JSON(http.StatusServiceUnavailable,gin.H{
-				"error":"invalid response from posts service",
+		resp, err := http.Get(POSTSAPI_URL + "/topPosts?limit=5")
+		if err != nil {
+			ctx.JSON(http.StatusServiceUnavailable, gin.H{
+				"error": "invalid response from posts service",
 			})
 			return
 		}
 		// defer resp.Body.Close()
-		
+
 		var posts []post
 		err1 := json.NewDecoder(resp.Body).Decode(&posts)
 		if err1 != nil {
-			fmt.Println("Error parsing json: ",err1)
-			ctx.JSON(http.StatusInternalServerError,gin.H{
-				"error":err1,
+			fmt.Println("Error parsing json: ", err1)
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": err1,
 			})
 			return
 		}
 
-		
 		// body, err := io.ReadAll(resp.Body)
 		// if err != nil {
 		// 	fmt.Println("Error parsing request body: ",err)
@@ -58,10 +58,8 @@ func main(){
 		// }
 		// // var posts []post
 		// json.Unmarshal(body, &posts)
-		
-		ctx.JSON(http.StatusOK,gin.H{
-			"trending":posts,
-		})
+
+		ctx.JSON(http.StatusOK, posts)
 	})
 	router.Run(":7002")
 }
