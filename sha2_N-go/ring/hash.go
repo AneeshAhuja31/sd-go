b5677a42ce0e516492549b23d4e1627ef22d68a3
Aneesh Ahuja <aneeshahuja31@gmail.com>
2026-01-26 08:43:25 +0530

    Implement Node creation function and add hashing utilities for ring management

---
+++ sha2_N-go/ring/hash.go
@@ -0,0 +1,15 @@
+package ring
+
+import (
+	"crypto/sha256"
+	"encoding/binary"
+)
+
+func Hash(id string)uint64{
+	hashBytes := sha256.Sum256([]byte(id))
+	return binary.BigEndian.Uint64(hashBytes[:8])
+}
+
+func GetSlot(hash uint64,n int) int {
+	return int(hash % uint64(n))
+}
--- sha2_N-go/ring/ring.go
+++ sha2_N-go/ring/ring.go
@@ -4,13 +4,21 @@ import (
 	"sha-go/node"
 	"sha-go/config"
 	"github.com/redis/go-redis/v9"
+	"context"
 )
 
 type Ring struct {
 	redisConn *redis.Client
+	ctx context.Context
 	ring []node.Node
 }
 
-func MakeRing()*ring{
-	
+func MakeRing()*Ring{
+	redisClient,ctx := config.InitRedis("localhost",6379)
+	newRing := make([]node.Node, 16)
+	return &Ring{
+		redisConn: redisClient,
+		ctx: ctx,
+		ring: newRing,
+	}
 }
