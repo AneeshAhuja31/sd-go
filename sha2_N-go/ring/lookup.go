528794d04f1bb8d9e10a8aae38e2527046bc4c4c
Aneesh Ahuja <aneeshahuja31@gmail.com>
2026-01-28 01:58:25 +0530

    Refactor FindNode function to return a node; add key hashing and slot calculation

--- sha2_N-go/ring/lookup.go
+++ sha2_N-go/ring/lookup.go
@@ -3,8 +3,27 @@ package ring
 import (
 	"fmt"
 	"log"
+	"sha-go/node"
 )
 
-func FindNode(key string, ring Ring){
-
+func FindNode(key string, shaRing Ring, n int)*node.Node{
+	keyHash := Hash(key)
+	keySlot := GetSlot(keyHash,n)
+	// var selectedNode node.Node
+	var firstNode *node.Node
+	if len(shaRing.Ring) > 0{
+		for _,currNode := range(shaRing.Ring){
+			if currNode.DB == nil{
+				continue
+			}
+			if firstNode == nil{
+				firstNode = &currNode
+			}
+			if currNode.Slot > keySlot{
+				return &currNode
+			}
+			return &currNode
+		}
+	}
+	return firstNode
 }
