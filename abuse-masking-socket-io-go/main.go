3db666aab65300ce75c8be87d43677674cbca9a4
Aneesh Ahuja <aneeshahuja31@gmail.com>
2026-02-03 04:20:18 +0530

    Add initial implementation of abuse masking server with socket.io

---
+++ abuse-masking-socket-io-go/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/googollee/go-socket.io"
+	"github.com/googollee/go-socket.io/engineio"
+)
+
+func main(){
+	server := socketio.NewServer(nil)
+	
+}
